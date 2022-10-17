b658342d7ad6cdcf96937da6990848d335afec58
KDepayso <krdepayso@gmail.com>
2022-10-17 16:20:33 +0100

    Stack Basics.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,43 @@
+import java.util.EmptyStackException;
+
 public class Main {
     public static void main(String[] args) {
-        MyNode<Integer> node1 = new MyNode<>(1);
-        MyNode<Integer> node2 = new MyNode<>(2);
-        MyNode<Integer> node3 = new MyNode<>(3);
+//        MyNode<Integer> node1 = new MyNode<>(1);
+//        MyNode<Integer> node2 = new MyNode<>(2);
+//        MyNode<Integer> node3 = new MyNode<>(3);
+//
+//        node1.setNext(node2);
+//        node2.setNext(node3);
+//
+//
+//        System.out.println("Value of node1 is " + node1.getData());
+//        System.out.println("Value of node2 is " + node1.getNext().getData());
+//        System.out.println("Value of node3 is " + node1.getNext().getNext().getData());
 
-        node1.setNext(node2);
-        node2.setNext(node3);
+        Stack<Integer> stack = new Stack<>();
 
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
 
-        System.out.println("Value of node1 is " + node1.getData());
-        System.out.println("Value of node2 is " + node1.getNext().getData());
-        System.out.println("Value of node3 is " + node1.getNext().getNext().getData());
+        for (int i = 1; i <= 4; i++){
+            try {
+                System.out.println("Peek = " + stack.peek());
+                System.out.println("Pop = " + stack.pop());
+            }
+            catch (EmptyStackException e ){
+                System.out.println("Exception");
+            }
+
+        }
+
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+
+        System.out.println("Stack empty is " + stack.isEmpty());
+        stack.clear();
+        System.out.println("Stack empty is " + stack.isEmpty());
 
     }
 }
---
+++ src/Stack.java
@@ -0,0 +1,39 @@
+import java.util.EmptyStackException;
+
+public class Stack<T> implements StackInterface<T> {
+
+    private MyNode<T> topNode;
+
+    public Stack() {
+        topNode = null;
+    }
+
+    public void push (T newEntry){
+        MyNode<T> newNode = new MyNode<>(newEntry);
+        newNode.setNext(topNode);
+        topNode = newNode;
+
+    }
+
+
+    public T pop(){
+        T dataToReturn = peek();
+        topNode = topNode.getNext();
+        return dataToReturn;
+    }
+
+    public T peek(){
+        if(topNode== null) throw new EmptyStackException();
+        else return topNode.getData();
+
+    }
+
+    public boolean isEmpty(){
+        return (topNode == null);
+    }
+
+    public void clear(){
+        topNode = null;
+
+    }
+}
